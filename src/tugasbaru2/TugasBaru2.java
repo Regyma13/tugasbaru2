e0bf3c01bc3927895378879bade5bfb12e9520a8
regyma <regymahmudazis@gmail.com>
2017-06-19 10:50:35 +0700

    Tugas 2

---
+++ src/tugasbaru2/TugasBaru2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasbaru2;
+
+/**
+ *
+ * @author Regyma
+ */
+public class TugasBaru2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Form Data = new Form();
+        Data.setVisible(true);
+    }
+    
+}
